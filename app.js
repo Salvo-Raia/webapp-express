63d9a586f8c6c1be42c457c0085abf16bfb13516
Salvo-Raia <salvo.raia@outlook.it>
2026-03-18 02:55:03 +0100

    Add: logger middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const logger = require("./middlewares/logger");
 const errorHandlers = require("./middlewares/errorHandlers");
 
 // Middlewares
+app.use(logger);
+app.use(express.static("public"));
+app.use(express.json());
 app.use(errorHandlers.notFound);
 app.use(errorHandlers.internalServerError);
 
